5b3efb633c1be3bbe57a840a2db2206c3a320123
james <james@JAMES-PC>
2022-03-13 21:52:47 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿class Person 
+{
+    static void Main(string[] args)
+    {
+        //Call the "Name" method defined above
+
+        Name();
+        MyInput();
+    }
+
+    static void Name() {
+        //Example of a simple method
+
+        var firstname = "James";
+        var secondname = "McConnell";
+
+        Console.WriteLine("My name is " + firstname + " " + secondname);
+  
+    }
+
+    static void MyInput() {
+
+        Console.WriteLine("Please enter your name: ");
+
+        string text_input = Console.ReadLine();
+
+        Console.WriteLine("Your name is " + text_input);
+        Console.ReadLine();
+    }
+
+  }
